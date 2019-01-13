e56738049922e84fb43e685001ea30cd467c0bab
MatthewHilleryJCU <matthew.hillery@my.jcu.edu.au>
2019-01-13 20:06:33 +1000

    Added open and new button functionality

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,11 +8,15 @@ import javafx.scene.control.*;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.stage.FileChooser.ExtensionFilter;
 
+import java.io.*;
 
 
 public class Main extends Application {
+    TextArea textArea = new TextArea();
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -22,44 +26,64 @@ public class Main extends Application {
         Scene scene = new Scene(borderPane, 800, 800);
 
 
-        // Menu
-        MenuBar menuBar = new MenuBar();
-
-        // File
-        Menu menuFile = new Menu("File");
-        MenuItem newFile = new MenuItem("New");
-        newFile.setAccelerator(KeyCombination.keyCombination("Ctrl+N"));
-        MenuItem openFile = new MenuItem("Open...");
-        openFile.setAccelerator(KeyCombination.keyCombination("Ctrl+O"));
-        MenuItem saveFile = new MenuItem("Save");
-        saveFile.setAccelerator(KeyCombination.keyCombination("Ctrl+S"));
-        MenuItem saveAsFile = new MenuItem("Save As...");
-        MenuItem pageSetupFile = new MenuItem("Page Setup...");
-        MenuItem printFile = new MenuItem("Print...");
-        printFile.setAccelerator(KeyCombination.keyCombination("Ctrl+P"));
-        MenuItem exitFile = new MenuItem("Exit");
-        menuFile.getItems().addAll(newFile, openFile, saveFile, saveAsFile,new SeparatorMenuItem(), pageSetupFile, printFile,new SeparatorMenuItem(), exitFile);
-
-        // Edit
-        Menu menuEdit = new Menu("Edit");
-
-        //Format
-        Menu menuFormat = new Menu("Format");
-
-        //View
-        Menu menuView = new Menu("View");
-
-        //Help
-        Menu menuHelp = new Menu("Help");
-
-        menuBar.getMenus().addAll(menuFile, menuEdit, menuFormat, menuView, menuHelp);
-        borderPane.setTop(menuBar);
-
-        TextArea textArea = new TextArea();
         textArea.borderProperty();
         borderPane.setCenter(textArea);
 
+        // Menu
+        MenuBar menuBar = new MenuBar();
 
+        // FILE
+        Menu menuFile = new Menu("File");
+
+            // New
+        MenuItem newFile = new MenuItem("New");
+        newFile.setAccelerator(KeyCombination.keyCombination("Ctrl+N"));
+        newFile.setOnAction (e -> textArea.setText(""));
+
+            //Open
+        MenuItem openFile = new MenuItem("Open...");
+        openFile.setAccelerator(KeyCombination.keyCombination("Ctrl+O"));
+        openFile.setOnAction (e -> openFilex (primaryStage));
+
+
+            //Save
+        MenuItem saveFile = new MenuItem("Save");
+        saveFile.setAccelerator(KeyCombination.keyCombination("Ctrl+S"));
+        saveFile.setOnAction(e -> );
+
+            //Save As
+        MenuItem saveAsFile = new MenuItem("Save As...");
+
+            //Page Setup
+        MenuItem pageSetupFile = new MenuItem("Page Setup...");
+
+            //Print
+        MenuItem printFile = new MenuItem("Print...");
+        printFile.setAccelerator(KeyCombination.keyCombination("Ctrl+P"));
+
+            //Exit
+        MenuItem exitFile = new MenuItem("Exit");
+
+            //Creates file menu items
+        menuFile.getItems().addAll(newFile, openFile, saveFile, saveAsFile,new SeparatorMenuItem(), pageSetupFile, printFile,new SeparatorMenuItem(), exitFile);
+
+        // EDIT
+        Menu menuEdit = new Menu("Edit");
+
+            //Format
+        Menu menuFormat = new Menu("Format");
+
+            //View
+        Menu menuView = new Menu("View");
+
+            //Help
+        Menu menuHelp = new Menu("Help");
+
+            //Creates file menu items
+        menuBar.getMenus().addAll(menuFile, menuEdit, menuFormat, menuView, menuHelp);
+
+
+        borderPane.setTop(menuBar);
         primaryStage.setScene(scene);
         primaryStage.show();
 
@@ -69,4 +93,68 @@ public class Main extends Application {
 
     public static void main(String[] args) { launch(args);
     }
+
+        private void openFilex (Stage stage) {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Opens");
+            fileChooser.getExtensionFilters().addAll(
+                    new ExtensionFilter("Text Files", "*.txt"));
+            File selectedFile = fileChooser.showOpenDialog(stage);
+            if (selectedFile != null) {
+//                System.out.println(selectedFile);
+                textArea.setText(openFileString(selectedFile));
+                String basename;
+                basename = selectedFile.getName();
+                stage.setTitle(basename);
+
+            }
+        }
+
+
+        private String openFileString (File file) {
+            // The name of the file to open.
+            String fileName = file.toString();
+
+            // This will reference one line at a time
+            String line = null;
+            String fullText = "";
+
+            try {
+                // FileReader reads text files in the default encoding.
+                FileReader fileReader =
+                        new FileReader(fileName);
+
+                // Always wrap FileReader in BufferedReader.
+                BufferedReader bufferedReader =
+                        new BufferedReader(fileReader);
+
+                while((line = bufferedReader.readLine()) != null) {
+                    System.out.println(line);
+                    fullText = fullText.concat(line + "\n");
+
+                }
+
+                // Always close files.
+                bufferedReader.close();
+            }
+            catch(FileNotFoundException ex) {
+                System.out.println(
+                        "Unable to open file '" +
+                                fileName + "'");
+            }
+            catch(IOException ex) {
+                System.out.println(
+                        "Error reading file '"
+                                + fileName + "'");
+                // Or we could just do this:
+                // ex.printStackTrace();
+            }
+
+            return fullText;
+        }
+
 }
+
+
+
+
